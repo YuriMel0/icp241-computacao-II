9186bb8c3edebdf386550b82f35fd4be4a2ad5fd
YuriMel0 <yurimelo216@gmail.com>
2022-12-17 19:54:09 -0300

    finalizando segunda questao da primeira lista de tkinter

--- source/exer8-tkinter/tkinter01/questao02.py
+++ source/exer8-tkinter/tkinter01/questao02.py
@@ -25,13 +25,13 @@ class ContaCorrente:
         return self.saldo
     
     def deposita(self, valor_deposito) -> None:
-        if valor_deposito < 0 or valor_deposito > self.limite:
+        if (float(valor_deposito) < 0) or (float(valor_deposito) > float(self.limite)):
             raise ValueError('O valor do deposito é invalido!')
         else:
             self.saldo += valor_deposito
 
     def sacar(self, valor_saque: float) -> None:
-        if valor_saque > self.saldo or valor_saque <= 0:
+        if (float(valor_saque) > float(self.saldo)) or float(valor_saque) <= 0:
             raise ValueError('O valor do saque é invalido')
         else:
             self.saldo -= valor_saque
@@ -49,11 +49,18 @@ class Janela:
         self.fr4 = Frame(tk)
         self.fr5 = Frame(tk)
 
+        self.fr6 = Frame(tk) # operações
+        self.fr7 = Frame(tk) # valor
+        self.fr8 = Frame(tk) # status
+
         self.fr1.pack()
         self.fr2.pack()
         self.fr3.pack()
         self.fr4.pack()
         self.fr5.pack()
+        self.fr6.pack()
+        self.fr7.pack()
+        self.fr8.pack()
 
         Label(self.fr1, fg='red', font=('Arial', 14, 'bold'),
               text="BANCO UFRJ", height=3).pack()
@@ -73,9 +80,47 @@ class Janela:
         Button(self.fr5, text='Criar Conta', fg='black', font=('Arial', 10, 'bold'),
                 padx=1, pady=1, command=self.criarConta).pack()
 
+        Label(self.fr6, fg='red', font=('Arial', 14, 'bold'), text='OPERAÇÔES', height=5).pack()
+        Label(self.fr7, text='Valor: ', fg='black', font=('Arial', 10, 'bold'), padx=30).pack(side='left')
+        self.e4 = Entry(self.fr7)
+        self.e4.pack()
+
+        Button(self.fr7, text='Saque', fg='black', font=('Arial', 10, 'bold'),
+                padx=1, pady=1, command=self.fazerSaque).pack(side='left')
+
+        Button(self.fr7, text='Deposito', fg='black', font=('Arial', 10, 'bold'),
+                padx=1, pady=1, command=self.fazerDeposito).pack(side='right')
+
+        self.status = Label(self.fr8, text='STATUS',fg='green', font=('Arial', 12, 'bold'), height=4)
+        self.status.pack()
+
     
     def criarConta(self):
-        pass
+        self.conta = ContaCorrente(self.e1.get(), self.e2.get(), self.e3.get())
+
+    def fazerSaque(self):
+        if (float(self.e4.get()) > self.conta.getSaldo()):
+            self.status['text'] = 'SALDO INSUFICIENTE'
+            self.status['fg'] = 'red'
+        else:
+            self.conta.sacar(float(self.e4.get()))
+            self.status['text'] = 'SAQUE REALIZADO COM SUCESSO'
+            self.status['fg'] = 'green'
+    
+    def fazerDeposito(self):
+        try:
+            if (float(self.e4.get()) > float(self.conta.getLimite())):
+                self.status['text'] = 'VALOR DO DEPOSITO É MAIOR QUE LIMITE DA CONTA'
+                self.status['fg'] = 'red'
+            else:
+                self.conta.deposita(float(self.e4.get()))
+                self.status['text'] = 'DEPOSITO REALIZADO COM SUCESSO'
+                self.status['fg'] = 'green'
+        except(ValueError, TypeError) as e:
+            self.status['text'] = str(e)
+            self.status['fg'] = 'red'
+            
+
 
 
 raiz = Tk()
