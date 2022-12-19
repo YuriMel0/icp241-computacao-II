6a08d6c0a9ec856fbf204f63046e4462b2299743
YuriMel0 <yurimelo216@gmail.com>
2022-12-19 15:33:02 -0300

    corrigindo primeira questão e criando segunda

--- source/exer11-matplotlib/questao01.py
+++ source/exer11-matplotlib/questao01.py
@@ -6,6 +6,6 @@ y1 = np.exp(x)
 y2 = 25*x-5
 y3=x**3+x**2
 plt.plot(x,y1,'bs-',x,y2,'g^-', x,y3,'r8-')
-plt.legend(['exponencial', 'linear', 'terceiro grau'], loc='best')
+plt.legend(['exponencial', 'linear', 'terceiro-grau'], loc='best')
 plt.grid(True)
 plt.show()
